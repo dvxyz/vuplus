a3b47161927125c1cdaccda7785da10e43c0beff
Vu+ <vuuno@velezschrod.xyz>
2018-12-02 00:53:08 +0100

    2018-12-02 00:53:08

--- gitcommit.sh
+++ gitcommit.sh
@@ -1,38 +1,9 @@
 #!/bin/bash
 
-ACTION="$1"
-DRIVE="$2"
-DEFAULT="Mount/Umount <hts|passport>"
+THEDATE=$(date '+%Y-%m-%d %H:%M:%S')
 
-# $ACTION /mnt/$DRIVE
+cd /home/vuuno/userbouquets
+git commit -am "$THEDATE" && git push
 
-if [[ "$ACTION" == "mount" || "$ACTION" == "umount" ]] ; then
-	case "$DRIVE" in
-		hts)
-			$ACTION /mnt/hts > /dev/null 2>&1 &	;;
-		passport)
-			$ACTION /mnt/passport > /dev/null 2>&1 & ;;
-		*) 
-			echo $DEFAULT ;;
-	esac
-else
-	echo $DEFAULT ;
-fi
-
-# case "$ACTION" in
-# 	mount) 
-# 		if [[ "$DRIVE" == "passport" ]] ; then
-# 			mount 192.168.0.200:/Volumes/My\ Passport\ Studio /mnt/passport
-# 		elif [[ "$DRIVE" == "hts" ]] ; then
-# 			mount /mnt/hts
-# 		fi
-# 	;;
-# 	umount)
-# 		if [[ "$DRIVE" == "passport" ]] ; then
-# 			umount /mnt/passport
-# 		elif [[ "$DRIVE" == "hts" ]] ; then
-# 			umount /mnt/hts
-# 		fi
-# 	;;
-# 	*)	echo "Mount/Umount <hts|passport>"
-# esac
+cd /home/vuuno/vuplus
+git commit -am "$THEDATE" && git push
