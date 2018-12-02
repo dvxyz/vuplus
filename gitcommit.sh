5ae8b94827c56f7a354fb0c2079ffb42ecfe4398
Vu+ <vuuno@velezschrod.xyz>
2018-12-02 01:29:02 +0100

    2018-12-02 01:29:01

--- gitcommit.sh
+++ gitcommit.sh
@@ -2,8 +2,12 @@
 
 THEDATE=$(date '+%Y-%m-%d %H:%M:%S')
 
-cd /home/vuuno/userbouquets
-git commit -am "$THEDATE" && git push
+# cd /home/vuuno/userbouquets
+# git commit -am "$THEDATE" && git push
+# 
+# cd /home/vuuno/vuplus
+# git commit -am "$THEDATE" && git push
 
-cd /home/vuuno/vuplus
-git commit -am "$THEDATE" && git push
+for d in $(dirname $(find $HOME/ -name .git -type d)) ; do 
+	cd "$d" && git commit -am "$THEDATE" && git push ;
+done
