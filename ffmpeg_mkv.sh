1ec623b6ee9d52020562a0ac9d18540f91361b74
Vu+ <vuuno@velezschrod.xyz>
2019-03-09 02:07:16 +0100

    2019-03-09 02:07:16

--- ffmpeg_mkv.sh
+++ ffmpeg_mkv.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
 
-ffmpeg -i $input -c copy $output
+FFMPEG="ffmpeg"
+ACODEC=copy
+VCODEC=copy
+OFFSET=00:00:00
+MV="mv"
+RM="rm"
+RECDIR="/mnt/usb/${1:-*}"
+SCRAMBLE=$(dirname "$RECDIR")/scrambled
+
+mkdir -p "$SCRAMBLE"
+
+for f in "$RECDIR" ; do
+ MKVDIR=$(dirname "$f") ;
+ MKVFILE=$(basename "$f" .ts).mkv ;
+ $FFMPEG -ss $OFFSET -y -i "$f" -map 0:v -map 0:a -c:v $VCODEC -c:a $ACODEC -sn "$MKVDIR/$MKVFILE" ;
+ if [[ $? -eq 0 ]] ; then 
+  $RM "$f"; 
+ else
+  echo "$f : RC $?" ;
+  $MV "$f" "$SCRAMBLE/" ; 
+  $RM "$MKVDIR/$MKVFILE" ; 
+ fi ;
+done
+
