4f95ed52f35c5281e064a2c45469b73b75a98aff
Vu+ <vuuno@velezschrod.xyz>
2019-03-10 06:00:04 +0100

    2019-03-10 06:00:02

--- ffmpeg_mkv.sh
+++ ffmpeg_mkv.sh
@@ -3,24 +3,32 @@
 FFMPEG="ffmpeg"
 ACODEC=copy
 VCODEC=copy
-OFFSET=00:00:00
+OFFSET=00:05:00
 MV="mv"
 RM="rm"
-RECDIR="/mnt/usb/${1:-*}"
-SCRAMBLE=$(dirname "$RECDIR")/scrambled
+RECDIR=$(dirname "${1:-/mnt/usb/.}")
+RECFILE=$(basename "${1:-*.*}" .ts)
+ARCHIVED="$RECDIR/ts"
+SCRAMBLE="$RECDIR/scrambled"
+MKVDIR=/mnt/passport/vuuno4k
 
+echo "$RECDIR"
+echo "$ARCHIVED"
+echo "$SCRAMBLE"
+
+mkdir -p "$ARCHIVED"
 mkdir -p "$SCRAMBLE"
 
-for f in "$RECDIR" ; do
- MKVDIR=$(dirname "$f") ;
- MKVFILE=$(basename "$f" .ts).mkv ;
+for f in "$RECDIR/$RECFILE" ; do
+ METADATA=$(basename "$f" .ts);
+ MKVFILE="$METADATA.mkv";
  $FFMPEG -ss $OFFSET -y -i "$f" -map 0:v -map 0:a -c:v $VCODEC -c:a $ACODEC -sn "$MKVDIR/$MKVFILE" ;
  if [[ $? -eq 0 ]] ; then 
-  $RM "$f"; 
+  $MV "$RECDIR/./$METADATA"*.{eit,ap,cuts,meta,sc,ts} "$ARCHIVED/"
  else
   echo "$f : RC $?" ;
-  $MV "$f" "$SCRAMBLE/" ; 
-  $RM "$MKVDIR/$MKVFILE" ; 
+  $MV "$RECDIR/./$METADATA"*.{eit,ap,cuts,meta,sc,ts} "$SCRAMBLE/";
+  $RM "$MKVDIR/$MKVFILE";
  fi ;
 done
 
