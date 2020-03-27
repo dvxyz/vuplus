1de824e657c8345e91621f580cae62d72de765ec
Vu+ <vuuno@velezschrod.xyz>
2020-03-27 06:00:11 +0100

    2020-03-27 06:00:01

--- iptables_rules.sh
+++ iptables_rules.sh
@@ -4,12 +4,12 @@ GENERALI=194.147.195.250
 DE=95.90.205.225
 UK=91.110.23.135
 
-if [ "$1" = "clear" ] ; then
- iptables -t filter -P OUTPUT ACCEPT
- iptables -t filter -P INPUT ACCEPT
- iptables -t filter -P FORWARD ACCEPT
- iptables -F
+iptables -t filter -P OUTPUT ACCEPT
+iptables -t filter -P INPUT ACCEPT
+iptables -t filter -P FORWARD ACCEPT
+iptables -F
 
+if [ "$1" = "clear" ] ; then
  echo "Cleared all rules."
  exit;
 fi
@@ -21,11 +21,11 @@ iptables -A INPUT -s 127.0.0.1 -j ACCEPT
 iptables -A INPUT -s 10.8.0.0/24 -j ACCEPT
 iptables -A INPUT -s 192.168.0.0/24 -j ACCEPT
 iptables -A INPUT -s 192.168.1.0/24 -j ACCEPT
-iptables -A INPUT -s ch.velezschrod.xyz -j ACCEPT
+# iptables -A INPUT -s ch.velezschrod.xyz -j ACCEPT
 iptables -A INPUT -s es.velezschrod.xyz -j ACCEPT
-iptables -A INPUT -s $UK -j ACCEPT
-iptables -A INPUT -s $DE -j ACCEPT
-iptables -A INPUT -s $GENERALI -j ACCEPT
+# iptables -A INPUT -s $UK -j ACCEPT
+# iptables -A INPUT -s $DE -j ACCEPT
+# iptables -A INPUT -s $GENERALI -j ACCEPT
 # iptables -A INPUT -m mac --mac-source 1c:b0:44:15:12:fc -j ACCEPT
 
 # for ip in $(egrep nameserver /etc/resolv.conf | cut -d ' ' -f 2) ; do
