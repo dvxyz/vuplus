16641d92d05100c628b48e8ebb558479dcd05aff
Vu+ <vuuno@velezschrod.xyz>
2020-10-13 06:00:17 +0200

    2020-10-13 06:00:01

--- iptables_rules.sh
+++ iptables_rules.sh
@@ -19,14 +19,11 @@ iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 iptables -A INPUT -i lo -j ACCEPT
 iptables -A INPUT -s 127.0.0.1 -j ACCEPT
 iptables -A INPUT -s 10.8.0.0/24 -j ACCEPT
+iptables -A INPUT -s 10.100.100.0/24 -j ACCEPT
 iptables -A INPUT -s 192.168.0.0/24 -j ACCEPT
 iptables -A INPUT -s 192.168.1.0/24 -j ACCEPT
 iptables -A INPUT -s ch.velezschrod.xyz -j ACCEPT
 iptables -A INPUT -s es.velezschrod.xyz -j ACCEPT
-
-# for ip in $(egrep nameserver /etc/resolv.conf | cut -d ' ' -f 2) ; do
-# iptables -A INPUT -p udp -s $ip --sport 53 -m state --state ESTABLISHED -j ACCEPT
-# done
 
 # default policies
 iptables -t filter -P OUTPUT ACCEPT
